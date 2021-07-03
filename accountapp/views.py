72b88df06b511092c99c43f5d67fc2aa42b3af8f
P-ws <pws12232002@gmail.com>
2021-07-03 12:47:53 +0900

    07/02 class 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html') #중간부분만 쓸거니까 이부분만 불러와줌
