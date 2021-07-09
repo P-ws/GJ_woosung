624ea9573a056e291d28eac4c44cb842a45a7954
P-ws <pws12232002@gmail.com>
2021-07-09 10:18:43 +0900

    1. post, get 형식으로 받을때를 구분하여 views.py 의 함수 수정 2. render에 context를 넣어줌으로 간편화 3. html에 중괄호 사용하면 context사용 가능

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html') #중간부분만 쓸거니까 이부분만 불러와줌
+    if request.method == "POST":   #POST으로 얻기
+        return render(request, 'accountapp/hello_world.html'
+                      , context={'text': 'POST METHOD!'}) #중간부분만 쓸거니까 이부분만 불러와줌
+
+    else:
+        return render(request, 'accountapp/hello_world.html'
+                      , context={'text': 'POST METHOD!'}) #이건 get으로 얻기
