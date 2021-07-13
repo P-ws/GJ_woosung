1bfa2e53ddb63aae72112ce5a5e91657470944c3
P-ws <pws12232002@gmail.com>
2021-07-13 09:51:57 +0900

    1. 데이터베이스 안의 모든 내용을 가져오기, data_list (views) 2. for을 돌려 다 출력 (중간 html, <h3>) 3. data_list란 이름으로 data_list를 값으로 설정

--- accountapp/views.py
+++ accountapp/views.py
@@ -15,11 +15,15 @@ def hello_world(request):
         model_instance.text = temp
         model_instance.save()    #데이터가 실제로 DB에 저장 됨 (이 저장된 데이터를 반환해줌)
 
+        #데이터베이스 안의 모든 내용을 가져옴
+        data_list = NewModel.objects.all()
+
 
         # 모델 데이터를 읽어옴
         return render(request, 'accountapp/hello_world.html'
-                      , context={'model_instance': temp}) #중간부분만 쓸거니까 이부분만 불러와줌
+                      , context={'data_list': data_list}) #중간부분만 쓸거니까 이부분만 불러와줌
 
     else:
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html'
-                      , context={'text': 'GET METHOD!'}) #이건 get으로 얻기
+                      , context={'data_list': data_list})  # 중간부분만 쓸거니까 이부분만 불러와줌
