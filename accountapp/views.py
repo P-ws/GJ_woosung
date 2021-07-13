7860c973a203677f2a7cdf66d8a101fb8f8e91b7
P-ws <pws12232002@gmail.com>
2021-07-13 09:22:58 +0900

    1. 중간 html에 버튼 말고 text 넣는 창 추가 2. view에서 model에 데이터베이스에 넣고 다시 가져오는거 까지 3. 데이터베이스 main 만들어주기

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import NewModel
+
 
 def hello_world(request):
     if request.method == "POST":   #POST으로 얻기
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()    #데이터가 실제로 DB에 저장 됨 (이 저장된 데이터를 반환해줌)
+
+
+        # 모델 데이터를 읽어옴
         return render(request, 'accountapp/hello_world.html'
-                      , context={'text': 'POST METHOD!'}) #중간부분만 쓸거니까 이부분만 불러와줌
+                      , context={'model_instance': temp}) #중간부분만 쓸거니까 이부분만 불러와줌
 
     else:
         return render(request, 'accountapp/hello_world.html'
