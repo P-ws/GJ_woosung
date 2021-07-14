4bae115e7613204348e5ac9cffd3ff8419e7136a
P-ws <pws12232002@gmail.com>
2021-07-14 14:53:55 +0900

    1. class를 만들어줌 2. 만든 class; urls로 설정

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import NewModel
 
@@ -24,3 +27,10 @@ def hello_world(request):
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html'
                       , context={'data_list': data_list})  # 중간부분만 쓸거니까 이부분만 불러와줌
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    templates_name = 'accountapp/create.html'
