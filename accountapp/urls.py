f8e43c98e2da009758478dc97dac31c5fe80ad29
P-ws <pws12232002@gmail.com>
2021-07-16 10:50:26 +0900

    1. class Detail생성 (View) 2. 경로에 detail경로 설정 3. template(accountapp)에 detail.html을 생성 내용은 create껄 복붙, 그리고 필요없는 form관련 구문들 삭제, target_user애들꺼 보이게 설정

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = 'accountapp'
 
@@ -14,5 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     # as.view 붙이면 class를 함수로봄?
-    path('create/', AccountCreateView.as_view(), name='create')
+    path('create/', AccountCreateView.as_view(), name='create'),
+
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
